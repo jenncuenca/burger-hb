19dd6065311df79e13ad066fc85eb7e066ec5154
Jennifer Cuenca <>
2018-05-05 02:24:53 -0400

    coded seeds.sql with intial burger entries for table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Avocado BLT Burger');
+INSERT INTO burgers (name) VALUES ('Blackbean Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Sauteed Mushroom with Blue Cheese Burger');
