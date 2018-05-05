be4e9503352026491e9d475164d8108e07f88c43
Jennifer Cuenca <>
2018-05-05 02:33:09 -0400

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Avocado BLT Burger');
-INSERT INTO burgers (name) VALUES ('Blackbean Veggie Burger');
-INSERT INTO burgers (name) VALUES ('Sauteed Mushroom with Blue Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado BLT Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blackbean Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sauteed Mushroom with Blue Cheese Burger');
